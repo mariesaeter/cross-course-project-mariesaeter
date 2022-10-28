441397e3ab9daeaa31ee9a0ef943b13900e008c3
mariesaeter <marie.saeter@gmail.com>
2022-10-28 16:15:06 +0200

    added color and size attributes

--- js/products/jacket-specific.js
+++ js/products/jacket-specific.js
@@ -1,5 +1,7 @@
 const infoContainer = document.querySelector(".infoContainer");
 const colorContainer = document.querySelector(".colorContainer");
+const sizeContainer = document.querySelector(".sizeContainer");
+
 const loader = document.querySelector(".loader");
 
 const queryString = document.location.search;
@@ -23,6 +25,7 @@ async function fetchJacket() {
 
     createHtml(jacketDetails);
     createColorArray(jacketDetails);
+    createSizeArray(jacketDetails);
   } catch (error) {
     console.log(error);
   }
@@ -41,25 +44,80 @@ function createHtml(jacketDetails) {
 }
 
 // function for creating an array of the color choices
+// heavily inspired by https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
 function createColorArray(jacketDetails) {
   const colorArray = jacketDetails.attributes[1].options;
+
   console.log(colorArray);
-
-  // creating radio buttons
-  for (let color of colorArray) {
-    let label = document.createElement("label");
-
-    let input = document.createElement("input");
-    input.type = "radio";
-    input.name = "color";
-    input.addEventListener("change", () => {
-      Object.keys(colorArray).forEach((color) => {
-        colorArray[color] = false;
-      });
-      colorArray[color] = true;
-    });
-    label.appendChild(input);
-
-    colorContainer.appendChild(label);
+  // create the radio inputs and group them
+  colorContainer.innerHTML = colorArray
+    .map(
+      (
+        color
+      ) => `<div class=${color}><input type="radio" name="color" value="${color}" id="${color}">
+                <label for="${color}"></label></div>`
+    )
+    .join(" ");
+  // change event for choice of radio button
+  const radio = document.querySelectorAll('input[name="color"]');
+  for (const radioButton of radio) {
+    radioButton.addEventListener("change", showSelected);
+  }
+  // output
+  function showSelected(e) {
+    console.log(e);
+    if (this.checked) {
+      document.querySelector(
+        "#color-output"
+      ).innerText = `Color: ${this.value}`;
+    }
   }
 }
+
+function createSizeArray(jacketDetails) {
+  const sizeArray = jacketDetails.attributes[0].options;
+
+  console.log(sizeArray);
+  // create the radio inputs and group them
+  sizeContainer.innerHTML = sizeArray
+    .map(
+      (
+        size
+      ) => `<div class=${size}><input type="radio" name="size" value="${size}" id="${size}">
+                  <label for="${size}">${size}</label></div>`
+    )
+    .join(" ");
+  // change event for choice of radio button
+  const radio = document.querySelectorAll('input[name="size"]');
+  for (const radioButton of radio) {
+    radioButton.addEventListener("change", showSelected);
+  }
+  // output
+  function showSelected(e) {
+    console.log(e);
+    if (this.checked) {
+      document.querySelector("#size-output").innerText = `Size: ${this.value}`;
+    }
+  }
+}
+
+// creating radio buttons
+// Inspiration: https://softauthor.com/javascript-dynamic-html-radio-button/
+// for (let color of colorArray) {
+//   let label = document.createElement("label");
+
+//   let input = document.createElement("input");
+//   input.type = "radio";
+//   input.name = "color";
+//   input.value =
+
+//   // input.addEventListener("change", () => {
+//   //   Object.keys(colorArray).forEach((color) => {
+//   //     colorArray[color] = false;
+//   //   });
+//   //   colorArray[color] = true;
+//   // });
+//   label.appendChild(input);
+
+//   colorContainer.appendChild(label);
+// }
